53f4cfc4e111d56bad35db31234e7de0e34c73cd
Lia Kartika <148621802+liakartikaprgmr@users.noreply.github.com>
2023-11-07 17:37:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function Login() {
+    var done= 0;
+    var username = document.login.username.value;
+    var username = document.login.password.value;
+
+    if (username) == "himasi" && password == "himasi"{
+        if(confirm("Apakah anda ingin login?")){
+           window.location.href = "https://github.com/liakartikaprgmr" 
+        } else {
+            alert("membtalkan login !");
+        }
+    } else{
+        if(done==0){
+            alert("username/password Salah!");
+        }
+    }
+}
+}
